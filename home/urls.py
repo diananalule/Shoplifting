6c2d43c33115fdf226190df03145ec3c99e87f00
magalaReuben <magalareuben60@gmail.com>
2024-03-13 03:50:30 +0300

    working on creating new student

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home, sign_in_user
+from .views import home, sign_in_user, log_out, add_student
 
 urlpatterns = [
     path('', home, name='home'),
-    path('sign_in', sign_in_user, name='sign_in')
+    path('sign_in', sign_in_user, name='sign_in'),
+    path('logout', log_out, name='logout'),
+    path('add_student', add_student, name='add_student')
 ]
--- home/views.py
+++ home/views.py
@@ -5,13 +5,17 @@ from django.contrib.auth import authenticate
 def home(request):
     return render(request, 'index.html')
 
+def log_out(request):
+    request.session.flush()
+    
+def add_student(request):
+    return render(request, 'pages/addStudent.html')
+
 def sign_in_user(request):
-    email = request.POST['email']   
+    username = request.POST['name']   
     password = request.POST['password']
-    print(email, password)
-    user = authenticate(request, email=email, password=password)
-    print(user)
+    user = authenticate(request, username=username, password=password)
     if user == None:
         return render(request, 'index.html')
     else:
-        return render(request, 'pages/home.html')
+        return render(request, 'pages/home.html', {'user': user})
